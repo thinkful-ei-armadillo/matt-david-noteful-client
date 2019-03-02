180e7f1a2693c9ced0c619dd773ecf450d84801c
Matt Nguyen <matth3wn@gmail.com>
2019-03-01 21:35:51 -0800

    finally fixed everything

--- src/Note/Note.js
+++ src/Note/Note.js
@@ -16,7 +16,7 @@ export default class Note extends React.Component {
     e.preventDefault()
     const noteId = this.props.id
 
-    fetch(`${config.API_ENDPOINT}/notes/${noteId}`, {
+    fetch(`${config.API_ENDPOINT}/api/notes/${noteId}`, {
       method: 'DELETE',
       headers: {
         'content-type': 'application/json',
@@ -24,9 +24,8 @@ export default class Note extends React.Component {
       },
     })
       .then(res => { 
-        console.log(res);
         if (!res.ok)
-        {console.log('Not ok')
+        {
           return Promise.reject(e)}
           else {
           console.log('OK')
